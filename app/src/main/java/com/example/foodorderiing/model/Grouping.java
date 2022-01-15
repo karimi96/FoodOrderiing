0114f6d2ae5e2008858e727bf6cca91552a91ec6
karimi <zmanyamyd5@gmail.com>
2022-01-16 00:45:01 +0330

    fix date

--- app/src/main/java/com/example/foodorderiing/model/Grouping.java
+++ app/src/main/java/com/example/foodorderiing/model/Grouping.java
@@ -15,11 +15,14 @@ public class Grouping {
     @ColumnInfo(name = "picture")
     public String picture;
 
+    public int picture2;
 
-    public Grouping(int grouping_id, String name, String picture) {
+
+    public Grouping(int grouping_id, String name, String picture,  int picture2) {
         this.grouping_id = grouping_id;
         this.name = name;
         this.picture = picture;
+        this.picture2 = picture2;
     }
 
     @Ignore
@@ -28,5 +31,10 @@ public class Grouping {
         this.picture = picture;
     }
 
+    @Ignore
+    public Grouping(String name, int picture2) {
+        this.name = name;
+        this.picture2 = picture2;
+    }
 
 }
