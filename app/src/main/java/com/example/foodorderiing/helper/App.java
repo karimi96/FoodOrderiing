679163f41acb4a50c4c2ede33a0ae75f28365c32
karimi <zmanyamyd5@gmail.com>
2022-01-16 00:34:02 +0330

    fix date in App class

--- app/src/main/java/com/example/foodorderiing/helper/App.java
+++ app/src/main/java/com/example/foodorderiing/helper/App.java
@@ -2,6 +2,12 @@ package com.example.foodorderiing.helper;
 
 import android.app.Application;
 import android.content.Context;
+import android.util.Log;
+
+import com.mohamadamin.persianmaterialdatetimepicker.utils.PersianCalendar;
+import com.mohamadamin.persianmaterialdatetimepicker.utils.PersianCalendarUtils;
+
+import java.util.Date;
 
 public class App extends Application {
     public static Context context;
@@ -10,5 +16,10 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
+
+        Log.e("qqqqapp", "onCreate now: " +PersianCalendarUtils.julianToPersian(new Date().getTime()));
+        Log.e("qqqqapp", "onCreate: 7 day ago: " +PersianCalendarUtils.julianToPersian(Tools.getDate(-7).getTime()) );
+
+
     }
 }
