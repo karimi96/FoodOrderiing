3ef4fe25721c87b561fc88751a61203073716bda
karimi <zmanyamyd5@gmail.com>
2022-01-29 21:05:59 +0330

    clean product model

--- app/src/main/java/com/example/foodorderiing/model/Grouping.java
+++ app/src/main/java/com/example/foodorderiing/model/Grouping.java
@@ -15,26 +15,18 @@ public class Grouping {
     @ColumnInfo(name = "picture")
     public String picture;
 
-    public int picture2;
 
 
-    public Grouping(int grouping_id, String name, String picture,  int picture2) {
+    public Grouping(int grouping_id, String name, String picture) {
         this.grouping_id = grouping_id;
         this.name = name;
         this.picture = picture;
-        this.picture2 = picture2;
     }
 
     @Ignore
     public Grouping(String name, String picture) {
         this.name = name;
         this.picture = picture;
-    }
-
-    @Ignore
-    public Grouping(String name, int picture2) {
-        this.name = name;
-        this.picture2 = picture2;
     }
 
 }
