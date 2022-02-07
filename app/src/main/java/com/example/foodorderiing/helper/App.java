fc8ffe43bde984bfacb78cf1031e1a2773277dc0
karimi <zmanyamyd5@gmail.com>
2022-02-07 07:35:42 +0330

    fix database

--- app/src/main/java/com/example/foodorderiing/helper/App.java
+++ app/src/main/java/com/example/foodorderiing/helper/App.java
@@ -1,46 +1,31 @@
 package com.example.foodorderiing.helper;
 
-import android.Manifest;
 import android.app.Application;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.os.Environment;
-import android.provider.Settings;
 import android.util.Log;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import com.mohamadamin.persianmaterialdatetimepicker.utils.PersianCalendar;
-import com.mohamadamin.persianmaterialdatetimepicker.utils.PersianCalendarUtils;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import javax.security.auth.login.LoginException;
+import com.example.foodorderiing.database.DatabaseHelper;
 
 public class App extends Application {
     public static Context context;
+    public static DatabaseHelper db;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        context = getApplicationContext();
+//        context = getApplicationContext();
+        context = this;
+        db = DatabaseHelper.getInstance(context);
+        Log.e("qqqqapp", "onCreate: app" );
 
-//        Log.e("qqqqapp", "onCreate now: " +PersianCalendarUtils.julianToPersian(new Date().getTime()));
-//        Log.e("qqqqapp", "onCreate: 7 day ago: " +PersianCalendarUtils.julianToPersian(Tools.getDate(-7).getTime()) );
-        Log.e("qqqqapp", "onCreate now: " + Tools.getCurrentDate());
-//        Log.e("qqqqapp", "onCreate: 7 day ago: " + Tools.getSevenDayAgo(-7) );
-        Log.e("qqqqapp", "onCreate: 7 day ago : " + Tools.getSevenDayAgo() );
-        Log.e("qqqqapp", "onCreate: thirty days ago : " + Tools.getThirtyDaysAgo() );
-        Log.e("qqqqapp", "onCreate: thirty days ago : " + Tools.getMonthName());
     }
+
+    public static DatabaseHelper getDatabase(){
+        if (!db.isOpen() || db == null){
+
+            db = DatabaseHelper.getInstance(context);
+        }
+        return db;
+    }
+
 }
